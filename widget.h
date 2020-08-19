4b37920c3d14ba3754ec25ba72c8eeab6b537287
pingjuu <pung0552@gmail.com>
2020-08-19 15:28:37 +0900

    Add headerfile

---
+++ widget.h
@@ -0,0 +1,42 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class widget; }
+QT_END_NAMESPACE
+
+class widget : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    widget(QWidget *parent = nullptr);
+    ~widget();
+    int money = {0};
+    //void changeMoney();
+
+private slots:
+    void changeMoney(int coin);
+
+    void on_pb10_clicked();
+
+    void on_pb50_clicked();
+
+    void on_pb100_clicked();
+
+    void on_pb500_clicked();
+
+    void on_pbCoffee_clicked();
+
+    void on_pbTea_clicked();
+
+    void on_pbMilk_clicked();
+
+    void on_pbReset_clicked();
+
+private:
+    Ui::widget *ui;
+};
+#endif // WIDGET_H
